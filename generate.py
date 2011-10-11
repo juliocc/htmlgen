6f7974d5b4f96f316455a9966c12958a34b03e46
Julio Castillo <juliocc@gmail.com>
2011-10-11 12:53:51 -0430

    fix race conditions

--- generate.py
+++ generate.py
@@ -5,65 +5,55 @@ import time
 import glob
 import os
 import pyinotify
+import datetime
+from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader, TemplateError
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+TEMPLATES_DIR = os.path.join(PROJECT_DIR, 'templates')
+OUTPUT_DIR = os.path.join(PROJECT_DIR, 'www')
 
 class EventHandler(pyinotify.ProcessEvent):
     def process(self, event):
         name = os.path.basename(event.pathname)
         if event.path == os.path.join(TEMPLATES_DIR, "layout"):
             render_all()
-        elif event.pathname[-5:] == ".html" and name[0] != '.':
+        elif name.endswith(".html") and name[0] != '.':
             render(name)
-
-    def process_IN_CREATE(self, event):
-        # print "Created:", event.pathname
-        self.process(event)
 
     def process_IN_MODIFY(self, event):
         # print "modified:", event.pathname
         self.process(event)
 
-
-PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
-TEMPLATES_DIR = os.path.join(PROJECT_DIR, 'templates')
-OUTPUT_DIR = os.path.join(PROJECT_DIR, 'www')
-
 env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
 def monitor():
     wm = pyinotify.WatchManager() # Watch Manager
-    mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY # watched events
     handler = EventHandler()
-    notifier = pyinotify.Notifier(wm, handler)
-    wdd = wm.add_watch(TEMPLATES_DIR, mask, rec=True)
+    notifier = pyinotify.Notifier(wm, handler, timeout=100)
+    wdd = wm.add_watch(TEMPLATES_DIR, pyinotify.ALL_EVENTS, auto_add=True, rec=True)
     print "Monitoring for changes in {}. Exit with Ctrl-C".format(TEMPLATES_DIR)
     notifier.loop()
-
-def template_test():
-    a = "Hola"
-    b = "Chao"
-    c = "Test 2"
-    return locals()
 
 def render(template_name):
     print "Rendering {}".format(template_name)
     start = time.time()
     template = env.get_template(template_name)
+    simplename = os.path.splitext(template_name)[0]
+    context = {"bodyclass": simplename}
+
+    function_name = "template_" + simplename
+    function = globals().get(function_name)
+    if function is not None:
+        print "calling template function {} for template {}".format(function_name, template_name)
+        newdata = function()
+        try:
+            context = dict(context.items() + newdata.items())
+        except AttributeError:
+            print ("===> Warning: template functions should return "
+                   "dict-like objects: {} returned {}").format(function_name, newdata.__class__.__name__)
+
     with open(os.path.join(OUTPUT_DIR, template_name), 'w') as f:
-        simplename = os.path.splitext(template_name)[0]
-        context = {"bodyclass": simplename}
-
-        function_name = "template_" + simplename
-        function = globals().get(function_name)
-        if function is not None:
-            print "calling template function {} for template {}".format(function_name, template_name)
-            newdata = function()
-            try:
-                context = dict(context.items() + newdata.items())
-            except AttributeError:
-                print "===> Warning: tempate functions should return dict-like objects: {} returned {}".format(function_name, newdata.__class__.__name__)
-
-
         try:
             template.stream(**context).dump(f, 'utf-8')
         except Exception, e:
@@ -71,8 +61,10 @@ def render(template_name):
             f.write("<pre>\n")
             traceback.print_exc(file=f)
             f.write("\n</pre>")
+        f.flush()
+
     elapsed = (time.time() - start) * 1000
-    print "Rendered {} in {:.2f} ms".format(template_name, elapsed)
+    print "  done in {:.2f} ms".format(elapsed)
 
 def render_all():
     print "Re-rendering all files"
